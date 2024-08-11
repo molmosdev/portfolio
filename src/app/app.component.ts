4cfe6bf0f7446a6a16277db405d27a113acc2556
molmos <marcolmosdev@gmail.com>
2024-08-12 01:38:41 +0200

    feat: logo and favicon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,7 +7,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  animations: [
+    trigger('verticalFadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(+20px)' }),
+        animate('0.3s ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-in', style({ opacity: 0, transform: 'translateY(+20px)' }))
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'portfolio';
